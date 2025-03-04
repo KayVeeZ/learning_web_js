7a5b12e55140962f592cc1697cf41764d55cf7fc
KayVeeZ <kshitijvashisth@gmail.com>
2025-03-04 15:40:09 +0000

    done intro to nextjs

---
+++ introNextJSvid121/first/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Problems Solved by nextjs</h1>
+      <ul>
+        <li>Full-stack solutions</li>
+        <li>File-based routing</li>
+        <li>additional features like Router from next/navigation</li>
+        <li>optimised rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
