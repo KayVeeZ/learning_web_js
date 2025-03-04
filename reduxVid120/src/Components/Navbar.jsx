555dba5f5f04af8b8fe399e8c0dd621659bada04
KayVeeZ <kshitijvashisth@gmail.com>
2025-03-04 15:02:59 +0000

    learnt how to use redux

---
+++ reduxVid120/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+
+
+const Navbar = () => {
+  const count = useSelector((state)=>state.counter.value)
+
+  return (
+    <div>
+      I am a NavBAr and counter is {count}
+    </div>
+  )
+}
+
+export default Navbar
