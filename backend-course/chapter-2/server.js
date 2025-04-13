58227942aacb26683de6471815855d6a35178f90
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-13 15:19:59 +0100

    learnt how to test endpoints

--- backend-course/chapter-2/server.js
+++ backend-course/chapter-2/server.js
@@ -6,6 +6,10 @@ const express = require('express');
 const app = express();
 const PORT = 8383;
 
+let data = {
+    name: "Kshitij"
+}
+
 // Endpoints - HTTP Verbs (methods/actions) and ROutes/Paths
 // the method informs the nature of request and the route is a further subdirectory
 // (basically we request the body of code to respond appropriately, and these locations and/or routes are called endpoints)
@@ -14,16 +18,21 @@ const PORT = 8383;
 app.get('/', (req, res) => {
     // this is endpoint -1
     console.log('yay, i hit an endpoint', req.method);
-    res.sendStatus(201); // 200 series means  success, 400 series means client error, 500 means error on server side
+    // res.sendStatus(201); // 200 series means  success, 400 series means client error, 500 means error on server side
+    res.send('<h1>Homepage</h1>')
 })
 
 app.get('/dashboard', (req, res) => {
     console.log('dashboard endpoint hit', req.method);
-    res.send('<h1>hello world-this is html now</h1><input/>');
+    res.send('<h1>Dashboard</h1>');
 }
 )
 
 // Type 2- API endpoints (non-visual)
+
+app.get('/api/data', (re,res) => {
+    res.send(data);
+});
 
 
 app.listen(PORT, () => {
