976a3f8f867c009cfd0b7e11dbd8d18cadea9ba1
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-14 16:50:45 +0100

    finished registration route

--- backend-course/chapter-3/src/routes/authRoutes.js
+++ backend-course/chapter-3/src/routes/authRoutes.js
@@ -8,13 +8,36 @@ const router = express.Router();
 // register a new user endpoint /auth/register
 router.post('/register', (req,res) => {
   const { username, password }= req.body;
-  console.log(username,password);
-  res.sendStatus(201);
+  const hashedPassword = bcrypt.hashSync(password, 8);
+  
+  console.log(hashedPassword);
+
+  // save a new user and hashed password to the db
+  try{
+    const insertUser = db.prepare(`INSERT INTO users(username, password)
+      VALUES(?, ?)`);
+    const result = insertUser.run(username, hashedPassword);
+
+    // now that we have a user, we want to add a first todo for them
+    const defaultTodo = "Hello :) add your first todo!";
+    const insertTodo = db.prepare(`
+      INSERT INTO todos(user_id,task)
+      VALUES (?, ?)`);
+      insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+      // create a token
+      const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'});
+      res.json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(503);
+  }
+  
 }
 );
 
 router.post('/login', (req,res) => {
-  
+  const hashedPassword = bcrypt.hashSync(password, 8);
 }
 );
 
