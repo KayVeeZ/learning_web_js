001f4218f49503ea91c264b1f93864ebdbcd8220
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-21 16:42:01 +0100

    completed smoljames backend course

--- backend-course/chapter-4/src/middleware/authMiddleware.js
+++ backend-course/chapter-4/src/middleware/authMiddleware.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken'
 
 function authMiddleware(req, res, next) {
-    const token = req.headers['authorization'];
+    const token = req.headers['authorization']
 
-    if (!token) { return res.status(401).send({ message: "No token provided" }) }
+    if (!token) { return res.status(401).json({ message: "No token provided" }) }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) { return res.status(401).json({ message: "Invalid token" }) }
 
-        req.userId = decoded.id;
+        req.userId = decoded.id
         next()
     })
 }
