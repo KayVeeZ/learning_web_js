2f78aca4c4711e9745446beac17849cc7be88964
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-23 12:50:59 +0100

    created routes in the basic node http server

--- backend-course/sheryian-crash-4hr/1_http_server/src/server.js
+++ backend-course/sheryian-crash-4hr/1_http_server/src/server.js
@@ -8,19 +8,25 @@ const server = http.createServer((req, res) => {
     {
         res.end(`
             <head><title>About Page</title></head>
-            <h2>about page</h2>`);
+            <h2>about page</h2>
+            <a href="/profile">Go to Profile</a><br/>
+                <a href="/">Go to Home</a>`);
     }
     if (req.url=="/profile")
         {
             res.end(`
                 <head><title>Profile Page</title></head>
-                <h2>profile page</h2>`);
+                <h2>profile page</h2>
+                <a href="/about">Go to About</a><br/>
+                <a href="/">Go to Home</a>`);
         }
     if (req.url=="/")
     {
         res.end(`
             <head><title>Home Page</title></head>
-            <h1>Hello world! Welcome to home Page! Method is ${met}</h1>`);
+            <h1>Hello world! Welcome to home Page! Method is ${met}</h1>
+            <a href="/about">Go to About</a><br/>
+            <a href="/profile">Go to Profile</a>`);
     }
     
 
