f7667fdec16ddee979ac6bbd56abda0b89672702
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-02 16:09:33 +0100

    started learning gsao

---
+++ work/learn-gsap/html/script.js
@@ -0,0 +1,18 @@
+gsap.to("#box1",{
+    x:1200,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"yellow",
+    borderRadius:"50%",
+    scale: 0.5
+})
+gsap.from("#box2",{
+    x:1200,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale: 1
+})
