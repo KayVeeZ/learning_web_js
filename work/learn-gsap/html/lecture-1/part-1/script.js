1cd1a3a59207095fad832d0192672d1625d32646
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-02 18:03:26 +0100

    reorganised structure

--- work/learn-gsap/html/lecture-1/part-1/script.js
+++ work/learn-gsap/html/lecture-1/part-1/script.js
@@ -5,7 +5,9 @@ gsap.to("#box1",{
     rotate:360,
     backgroundColor:"yellow",
     borderRadius:"50%",
-    scale: 0.5
+    scale: 0.5,
+    repeat: -1,
+    yoyo: true
 })
 gsap.from("#box2",{
     x:1200,
@@ -14,5 +16,18 @@ gsap.from("#box2",{
     rotate:360,
     backgroundColor:"blue",
     borderRadius:"50%",
-    scale: 1
+    scale: 1,
+    repeat: -1,
+    yoyo: true
+})
+
+gsap.from("h1",{
+    color: "red",
+    duration: 1,
+    delay: 1,
+    opacity: 0,
+    y: 20,
+    stagger: 0.3,
+    repeat: -1,
+    yoyo: true
 })
