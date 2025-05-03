fe3f5e5df8de832342ca571e6a76b11103ad5a9e
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-03 15:38:30 +0100

    learning scroll trigger

---
+++ work/learn-gsap/basics/lecture-2/part-1/script.js
@@ -0,0 +1,14 @@
+var tl = gsap.timeline();
+gsap.from("#page1 #box",{
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360
+});
+gsap.from("#page2 #box",{
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360,
+    scrollTrigger:"#page2 #box"
+});
