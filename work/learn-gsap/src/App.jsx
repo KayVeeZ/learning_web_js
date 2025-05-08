22d3d97cd3c79b813797f900c86e44e17d795a74
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-08 22:10:36 +0100

    finshed react with gsap

--- work/learn-gsap/src/App.jsx
+++ work/learn-gsap/src/App.jsx
@@ -1,27 +1,39 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 const App = () => {
+  const [xValue, setXValue] = useState(0);
+  const randomX = gsap.utils.random(-500,500,100);
+  const rotateX = gsap.utils.random(-360,360,30);
   const boxRef = useRef();
+  const boxRef1 = useRef();
+  const [roti, setroti] = useState(0);
+  const { contextSafe } = useGSAP();
+
+  const rotateBox = contextSafe(function () {
+    gsap.to(boxRef1.current, {
+      rotate:360,
+      duration: 1
+    })
+  })
 
   useGSAP(()=>{
     gsap.to(boxRef.current,{
-      duration:1,
-      rotate: 360,
-    });
-  });
-
+      x:xValue,
+      duration: 1,
+      rotate:roti,
+    })
+  },{scope:'main', dependencies:[xValue, rotateX]});
   return (
     <main>
-      <div className="container">
-        <div className="circle"></div>
-        <div ref={boxRef} className="box"></div>
-      </div>
-      <div className="kuch">
-        <div className="circle"></div>
-        <div className="box"></div>
-      </div>
+      <button onClick={()=>{
+        setXValue(randomX);
+        setroti(rotateX);
+        rotateBox();
+      }}>Animate</button>
+      <div ref={boxRef} className="box"></div>
+      <div ref={boxRef1} className="box"></div>
     </main>
   )
 }
